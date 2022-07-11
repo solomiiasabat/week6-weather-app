34d741cd34d07fb673439e4c5134e3c3d5d4c90e
solomiyasabat <solomiyasabat@gmail.com>
2022-07-11 13:14:06 +0300

    finished js features

--- index.js
+++ index.js
@@ -34,15 +34,44 @@ let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = `${whatDay}, ${whatMonth} ${whatDate}, ${whatHour}:${whatMinutes}`;
 
 //Challenge 2
+
+function defaultCity(city) {
+  let apiKey = "8542913933d6e9526040ad6e6691ada1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function displayWeather(response) {
-  console.log(response.data.name);
+  document.querySelector("#chosen-city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchLocation(position) {
+  let apiKey = "8542913933d6e9526040ad6e6691ada1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 function search(event) {
   event.preventDefault();
-  let apiKey = "8542913933d6e9526040ad6e6691ada1";
-  let enterCity = document.querySelector("#weather-engine").value;
-
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${enterCity}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayWeather);
+  let city = document.querySelector("#weather-engine").value;
+  defaultCity(city);
 }
+let userInfo = document.querySelector("#search-line");
+userInfo.addEventListener("submit", search);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+defaultCity("Kyiv");
